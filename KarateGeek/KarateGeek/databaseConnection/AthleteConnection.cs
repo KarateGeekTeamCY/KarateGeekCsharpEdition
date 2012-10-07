a81394eef972c8bd39a8aa7b2df413054e3661f8
michael theodorides <mc.theodorides@gmail.com>
2012-10-07 12:15:11 +0300

    added update function in athlete

--- KarateGeek/KarateGeek/databaseConnection/AthleteConnection.cs
+++ KarateGeek/KarateGeek/databaseConnection/AthleteConnection.cs
@@ -33,9 +33,9 @@ namespace KarateGeek.databaseConnection
 
 
 
-            sql = "insert into table Persons ( id, first_name, middle_name, last_name, "+
-                "date_of_birth, primary_phone_number, secondary_phone_number, email, "+
-                "address_id) values ( '" 
+            sql = "insert into table Persons ( id, first_name, middle_name, last_name, " +
+                "date_of_birth, primary_phone_number, secondary_phone_number, email, " +
+                "address_id) values ( '"
                 + personId + "', '"
                 + firstName + "', '"
                 + middleName + "', '"
@@ -67,6 +67,77 @@ namespace KarateGeek.databaseConnection
                 + addressStreetNumber + "', '"
                 + addressPostalCode + "', '"
                 + countryCode + "' );";
+
+            this.NonQuery(sql);
+
+
+            return true;
+        }
+
+
+
+        public Boolean UpdateAthlete(string id, string firstName, string middleName, string lastName,
+           DateTime dateOfBirth,
+           string primaryPhoneNo, string secondaryPhoneNo, string email,
+           string countryCode, string City, string addressStreetName, string addressStreetNumber, string addressPostalCode,
+           string rank, string localClubId)
+        {
+            string sql = "";
+            NpgsqlDataReader dr = null;
+
+            //// getting the next person id
+            //sql = "select nextval('seq_persons_id'); ";
+            //dr = this.Query(sql);
+            //dr.Read();
+            //int personId = dr.GetInt32(0);
+
+            //// getting the next address id
+            //sql = "select nextval('seq_address_id'); ";
+            //dr = this.Query(sql);
+            //dr.Read();
+            //int addressId = dr.GetInt32(0);
+
+            //note
+            //UPDATE table_name
+            //SET column1=value, column2=value2,...
+            //WHERE some_column=some_value
+
+            sql = "update Persons set " +
+
+                "middle_name = '" + middleName + "', " +
+                "last_name = '" + lastName + "', " +
+                "date_of_birth = '" + dateOfBirth.ToShortDateString() + "', " +
+                "primary_phone_number = '" + primaryPhoneNo + "', " +
+                "secondary_phone_number = '" + secondaryPhoneNo + "', " +
+                "email = '" + email + "' where id = '" + id + "' ;";
+
+            this.NonQuery(sql);
+
+
+
+            sql = "update athlets set " +
+
+                "rank = '" + rank + "', " +
+                "local_association_id = '" + localClubId + "' where id = '" + id + "' ;";
+
+            this.NonQuery(sql);
+
+
+            // getting the address id
+            sql = "select address_id from persons where id = '" + id + "'; ";
+            dr = this.Query(sql);
+            dr.Read();
+            int addressId = dr.GetInt32(0);
+
+
+
+            sql = "update addresses set " +
+
+                "city = '" + City + "', " +
+                "street_name = '" + addressStreetName + "', " +
+                "street_number = '" + addressStreetNumber + "', " +
+                "street_postal_code = '" + addressPostalCode + "', " +
+                "country_code = '" + countryCode + "' where id = '" + addressId + "' ;";
 
             this.NonQuery(sql);
 
