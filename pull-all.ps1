3a5aeabd3ad445cd88ecdbff9f553edd121510e9
michael theodorides <mc.theodorides@gmail.com>
2012-10-07 11:38:53 +0300

    new pull all script

---
+++ pull-all.ps1
@@ -0,0 +1,11 @@
+git checkout master
+git pull
+git checkout michael-t
+git pull
+git checkout athina 
+git pull
+git checkout michael-a 
+git pull
+git checkout nikolas 
+git pull
+git checkout michael-t
