fa524c6ea49221083b992340a8fd68eb80b4b3a0
michael theodorides <mc.theodorides@gmail.com>
2012-10-07 11:45:49 +0300

    new merge all script

---
+++ merge-all.ps1
@@ -0,0 +1,32 @@
+git checkout master
+git pull
+git checkout michael-t
+git pull
+git checkout athina 
+git pull
+git checkout michael-a 
+git pull
+git checkout nikolas 
+git pull
+git checkout master
+git merge michael-t
+git merge michael-a
+git merge nikolas
+git merge athina
+git push
+
+git checkout athina 
+git merge master
+git push
+
+git checkout michael-a 
+git merge master
+git push
+
+git checkout nikolas 
+git merge master
+git push
+
+git checkout michael-t
+git merge master
+git push
