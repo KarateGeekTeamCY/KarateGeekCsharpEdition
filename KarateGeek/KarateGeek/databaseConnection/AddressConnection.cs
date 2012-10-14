3a012eafcd4b0d28f9aef5289f8a06191f2ba361
mixalis anastasiou <olympiakos8@gmail.com>
2012-10-15 02:49:55 +0300

    allages gia tin eisagwi twn stoixeiwn tou athliti
    ekana kapoies allages kai sto erwtima to insert kai sto kai ston kwdika
    stis sinartiseis ta pedia pou pernane!t erwtima den trexei kanonika
    giati leei kati gia foreign key sto country_code.

--- KarateGeek/KarateGeek/databaseConnection/AddressConnection.cs
+++ KarateGeek/KarateGeek/databaseConnection/AddressConnection.cs
@@ -8,17 +8,17 @@ namespace KarateGeek.databaseConnection
 {
     class AddressConnection : CoreDatabaseConnection
     {
-        public string InsertNewAddress(string countryCode, string City, string addressStreetName, string addressStreetNumber, string addressPostalCode)
+        public string InsertNewAddress(string addressStreetName,string addressStreetNumber,string City,string addressPostalCode,string countryCode)
         {
             string sql;
             NpgsqlDataReader dr = null;
 
-            sql = "insert into table addresses (city, street_name, street_number, " +
-                "street_postal_code, country_code ) values ( '"
-                + City + "', '"
+            sql = "insert into addresses (street, number, city," +
+                "postal_code, country_code ) values ( '"
                 + addressStreetName + "', '"
                 + addressStreetNumber + "', '"
-                + addressPostalCode + "', '"
+                + City + "', '" 
+                + addressPostalCode + "','"
                 + countryCode + "' );";
 
             this.NonQuery(sql);
