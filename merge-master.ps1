3482a322a1928b4b638896838ff1a6b22110b1cd
michael theodorides <mc.theodorides@gmail.com>
2012-10-15 17:05:00 +0300

    add script

---
+++ merge-master.ps1
@@ -0,0 +1,26 @@
+git checkout master
+git pull
+git checkout michael-t
+git pull
+git checkout athina 
+git pull
+git checkout michael-a 
+git pull
+git checkout nikolas 
+git pull
+
+git checkout athina 
+git merge master
+git push
+
+git checkout michael-a 
+git merge master
+git push
+
+git checkout nikolas 
+git merge master
+git push
+
+git checkout michael-t
+git merge master
+git push
