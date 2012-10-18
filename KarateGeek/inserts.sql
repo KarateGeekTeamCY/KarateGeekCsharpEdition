ddca4998851195657de0a20479e82f1ada52e847
michael theodorides <mc.theodorides@gmail.com>
2012-10-19 01:48:09 +0300

    rearaing helping selects

--- KarateGeek/inserts.sql
+++ KarateGeek/inserts.sql
@@ -8,15 +8,28 @@ insert into cities (name, country_code) values ('Larnaca', 'CY');
 insert into cities (name, country_code) values ('Amochostos', 'CY');
 insert into cities (name, country_code) values ('Thessaloniki', 'GR');
 
+
   
-insert into addresses (id , street, "number", city_id , postal_code , country_code ) values ('0' , 'teapk' , '10' , 1 , '3025' , 'CY'); 
+insert into addresses (id , street, "number", city_id , postal_code , country_code ) values ('0' , 'teapk' , '1' , '7' , '3025' , 'CY'); 
 
 
 insert into clubs (name, address_id, country_code) values ('power fight club', 0, 'CY');
 
 
-insert into persons(id , first_name, middle_name, last_name, date_of_birth, phone, secondary_phone, email, address_id) 
-values ('0' , 'administrator' , null , 'administrator', '02-10-1990' , '99123144' , null , 'email@gmail.com' , '0');
+insert into persons(id , first_name, fathers_name, last_name, date_of_birth, sex,  phone, secondary_phone, email, address_id) 
+values ('0' , 'administrator' , 'xampis' , 'administrator', '02-10-1990' , 'male', '99123144' , null , 'email@gmail.com' , '0');
 
-insert into athletes (id, rank, club_id ) values (1, 'black', 1  );
+insert into athletes (id, rank, club_id ) values ('0', 'black', '2'  );
 
+
+
+select * from cities;
+
+
+select * from addresses;
+
+
+select * from persons;
+
+
+select * from clubs;
