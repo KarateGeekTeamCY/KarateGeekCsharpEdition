069f4917eb49e3ccca41213baa97909ca7ce5ce0
michael theodorides <mc.theodorides@gmail.com>
2012-10-26 02:15:46 +0300

    mike part

---
+++ KarateGeek/KarateGeek/databaseConnection/ScoreConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KarateGeek.databaseConnection
+{
+    class ScoreConnection : CoreDatabaseConnection
+    {
+        private string sql;
+
+
+        public string InsertNewScore(string gameId, string athleteId, string technical)
+        {
+            sql = "insert into gane_scores (game_id, athlete_id, technical_score) values ('" + gameId + "', '" + athleteId + "', '" + technical + "');";
+            this.NonQuery(sql);
+            return "";
+        }
+
+        public string InsertNewScore(string gameId, string athleteId, string judge1Id, string judge2Id, string judge3Id, string judge4Id, string judge5Id, 
+            string score1, string score2, string score3, string score4, string score5)
+        {
+            sql = "insert into gane_scores (game_id, athlete_id, head_judge_id, judge1, judge2, judge3, judge4, "+
+                "head_score, score1, score2, score3, score4) values ('" + 
+                gameId + "', '" + athleteId + "', '" + 
+                judge1Id + "', '" + judge2Id + "', '" + judge3Id + "', '" + judge4Id + "', '" + judge5Id + "', '" + 
+                score1 + "', '" + score2 + "' ,'" + score3 + "' ,'" + score4 + "' ,'" + score5 + "');";
+
+            this.NonQuery(sql);
+            return "";
+        }
+
+
+
+
+
+    }
+}
