3cfb742c5b32f9c33aead53af2e9d26e31215e33
michael theodorides <mc.theodorides@gmail.com>
2012-11-01 10:08:02 +0200

    flag scoring system gui clean up and code preparetion

--- KarateGeek/KarateGeek/guis/FlagSystem.xaml.cs
+++ KarateGeek/KarateGeek/guis/FlagSystem.xaml.cs
@@ -29,9 +29,80 @@ namespace KarateGeek.guis
 
         }
 
-        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        #region color choosing button listeners
+
+        private void reda_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
+        private void whitea_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void redb_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void whiteb_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void redc_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void whitec_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void redd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void whited_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rede_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void whitee_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+
+        #region menu buttons save back etc
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+
     }
 }
