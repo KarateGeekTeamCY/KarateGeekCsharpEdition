33ced8f0fa47a42b2b5c4bce79c33002a575421b
michael theodorides <mc.theodorides@gmail.com>
2012-11-02 20:01:13 +0200

    kumite system part 01

--- KarateGeek/KarateGeek/guis/KumiteSystem.xaml.cs
+++ KarateGeek/KarateGeek/guis/KumiteSystem.xaml.cs
@@ -19,14 +19,134 @@ namespace KarateGeek.guis
     /// </summary>
     public partial class KumiteSystem : Window
     {
+
+        #region private declaretions
+
+        private int pointsIndex = 0;
+        private LinkedList<Point> pointsLeft = new LinkedList<Point>();
+        private LinkedList<Point> pointsRight = new LinkedList<Point>();
+
+
+
+
+
+        #endregion 
+
+
+
         public KumiteSystem()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        #region point buttons
+
+
+        private void ipponA_Click(object sender, RoutedEventArgs e)
+        {
+            Point p = new Point(Strings.ippon, 1, pointsIndex);
+            pointsIndex++;
+            this.pointsLeft.AddLast(p);
+
+        }
+
+        private void wazaariA_Click(object sender, RoutedEventArgs e)
+        {
+            Point p = new Point(Strings.wazaari, 1, pointsIndex);
+            pointsIndex++;
+            this.pointsLeft.AddLast(p);
+        }
+
+        private void jyogaiA_Click(object sender, RoutedEventArgs e)
+        {
+            Point p = new Point(Strings.jyogai, 1, pointsIndex);
+            pointsIndex++;
+            this.pointsLeft.AddLast(p);
+        }
+
+        private void chuiA_Click(object sender, RoutedEventArgs e)
+        {
+            Point p = new Point(Strings.chui, 1, pointsIndex);
+            pointsIndex++;
+            this.pointsLeft.AddLast(p);
+        }
+
+        private void keikokuA_Click(object sender, RoutedEventArgs e)
+        {
+            Point p = new Point(Strings.keikoku, 1, pointsIndex);
+            pointsIndex++;
+            this.pointsLeft.AddLast(p);
+        }
+
+        private void tentoA_Click(object sender, RoutedEventArgs e)
+        {
+            Point p = new Point(Strings.tento, 1, pointsIndex);
+            pointsIndex++;
+            this.pointsLeft.AddLast(p);
+        }
+
+        private void doctorStopA_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
+        private void ipponB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void wazaariB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void jyogaiB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chuiB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void keikokuB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tentoB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void doctorstopB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
     }
+
+
+    public class Point
+    {
+
+        public string descreption { get; set; }
+        public int points { get; set; }
+        public int pointsIndex { get; set; }
+
+        public Point(string dsc, int points, int PointIndex)
+        {
+            this.descreption = dsc;
+            this.points = points;
+            this.pointsIndex = pointsIndex;
+        }
+        
+    }
+
+
+
 }
