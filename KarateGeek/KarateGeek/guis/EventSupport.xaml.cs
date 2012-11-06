e16ed4bdfbbf34604b5ca5405c5809bcc8067449
michael theodorides <mc.theodorides@gmail.com>
2012-11-06 23:42:47 +0200

    event support gui part 02

--- KarateGeek/KarateGeek/guis/EventSupport.xaml.cs
+++ KarateGeek/KarateGeek/guis/EventSupport.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using KarateGeek.databaseConnection;
+using System.Data;
 
 namespace KarateGeek.guis
 {
@@ -18,9 +20,57 @@ namespace KarateGeek.guis
     /// </summary>
     public partial class EventSupport : Window
     {
+
+
+        #region private declaretions
+
+        private int  _tournamentId;
+
+        private bool _isTeam;
+        private string _gameType;
+        private string judgingType;
+        private TournamentConnection _tournamentConn;
+        private TournamentGameParticipationsConnection _tournamentParticipationConn;
+        private EventConnection _eventConn;
+        private List<string> _todaysEventTournaments = new List<string>();
+
+
+
+
+        #endregion private declaretions
+
+
+
+
         public EventSupport()
         {
             InitializeComponent();
+            
+
+
+
+
+
+
+
         }
+        
+
+
+
+
+
+        #region buttons
+        private void btnStartNextGame_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
     }
 }
