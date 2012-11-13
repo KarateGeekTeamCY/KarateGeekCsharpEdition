68457d983730947dd736871d45f5a99310d2675d
michael theodorides <mc.theodorides@gmail.com>
2012-11-13 17:39:47 +0200

    added user management

--- KarateGeek/KarateGeek/databaseConnection/UserConnection.cs
+++ KarateGeek/KarateGeek/databaseConnection/UserConnection.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Security.Cryptography;
 using KarateGeek.helpers;
+using System.Data;
 
 namespace KarateGeek.databaseConnection
 {
@@ -18,23 +19,22 @@ namespace KarateGeek.databaseConnection
         {
             defPassword = cr.cryptography(defPassword);
 
-            sql = "insert into users(id , username ,password, person_management, event_management , lottery , game_support , reports , settings)"
-                    + "values('0', '" + defUsername + "' , '" + defPassword + "' , '1' , '1' , '1' , '1' , '1' , '1');";
+            sql = "insert into users(username ,password, person_management, event_management , lottery , game_support , reports , settings)"
+                    + "values('" + defUsername + "' , '" + defPassword + "' , '1' , '1' , '1' , '1' , '1' , '1');";
 
             this.NonQuery(sql);
 
         }
 
 
-        public void insertNewUser(string personid, string username, string password, 
+        public void insertNewUser(string username, string password,
             bool personManagement, bool eventManagement, bool lottery, bool gameSupport, bool reports, bool settings)
         {
             password = cr.cryptography(password);
 
-            sql = "insert into users(id , username ,password, person_management, event_management , lottery , game_support , reports , settings) values ('" + 
-                    personid + "', '" + 
-                    username + "' , '" + 
-                    password + "' , '" + 
+            sql = "insert into users(username ,password, person_management, event_management , lottery , game_support , reports , settings) values ('" +
+                    username + "' , '" +
+                    password + "' , '" +
                     personManagement + "' , '" +
                     eventManagement + "' , '" +
                     lottery + "' , '" +
@@ -44,6 +44,11 @@ namespace KarateGeek.databaseConnection
 
             this.NonQuery(sql);
 
+        }
+
+        public DataTable findSimilar(string like)
+        {
+            return this.Query("select * from users where username like '" + like + "%'; ").Tables[0];
         }
 
 
